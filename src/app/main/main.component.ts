ec19e63650cf52f1928de8c12ced470d4dea8e0b
Ali Rıza Darcan <99339675+alidarcan@users.noreply.github.com>
2024-03-19 04:13:33 +0300

    (click) ngif ngfor

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
-
+  items: any[] = [1, 2, 3, 'hello', 'mf'];
+  counter: number = 0;
+  onClick() {
+    this.counter++;
+    console.log('clicked', this.counter, 'times');
+  }
 }
