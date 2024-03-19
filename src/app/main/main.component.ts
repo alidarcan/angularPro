ecfdcd35655694dd2ec325dea3451cd4cdd687ff
Ali Rıza Darcan <99339675+alidarcan@users.noreply.github.com>
2024-03-19 22:59:32 +0300

    [ngStyle]="{'color': colorPicked}" [(ngModel)]="colorPicked"

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent {
   items: any[] = [1, 2, 3, 'hello', 'mf'];
   counter: number = 0;
+  key: string = '';
   onClick() {
     this.counter++;
     console.log('clicked', this.counter, 'times');
-  }
-  key: string = '';
+  };
   onKeyPress(event: any) {
     console.log(event.key);
     this.key = event.key;
   }
+  colorPicked = "green";
+  fontSize = 20;
+
 }
